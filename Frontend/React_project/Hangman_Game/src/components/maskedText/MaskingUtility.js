947c9188c392d2491aae62ab934e9ce567579b0e
simran <shahsimran453@gmail.com>
2024-09-24 15:48:10 +0530

    logic for masked text

---
+++ Frontend/React_project/Hangman_Game/src/components/maskedText/MaskingUtility.js
@@ -0,0 +1,25 @@
+/** 
+ * @param {The word which is given as input and is expected to be guessed} originalWords 
+ * @param {Letters which are guessed by the user alraedy} guessedLetters 
+ * example:
+ * originalWord:Mango
+ * guessedLatters:['M','N','O']
+ * return->"M_N_O"
+ * **/
+export function getMaskedString(originalWords,guessedLetters){
+guessedLetters=guessedLetters.map(Letter=>Letter.toUpperCase());//converts all the guessedLetters to Uppercase
+
+const  guessedLetterSet=new Set(guessedLetters);  //sets of guessedLetters by using Set to make searching fast
+
+//split(),splits string into array form
+const result = originalWords.toUpperCase().split('').map(char => {
+    if (guessedLetterSet.has(char)) {
+      return char;
+    } else {
+      return "_";
+    }
+  })   //['M','_','N','_','O']
+  return result.join('');  //it joins array into string "M_N_O"
+}
+
+
