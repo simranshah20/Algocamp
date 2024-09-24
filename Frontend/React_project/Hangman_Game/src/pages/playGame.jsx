42b3e4be3b85c4f86cb8ad6d511ae1f86958dd9d
simran <shahsimran453@gmail.com>
2024-09-24 16:13:16 +0530

    Added Masked text

--- Frontend/React_project/Hangman_Game/src/pages/playGame.jsx
+++ Frontend/React_project/Hangman_Game/src/pages/playGame.jsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
+import Maskedtext from "../components/maskedText/MaskedText";
 
 function PlayGame(){
 //   const [searchParams]=useSearchParams();
@@ -7,9 +8,16 @@ function PlayGame(){
 //const {text}=useParams();  //fetching the value of text
 
 const  {state}=useLocation();  //destructing state property
+// const arr=["hello","world"];
     return(
         <>
         <h1>Play Game {state.wordSelected}</h1>  
+
+        {/* rendering list in reacct (on UI(using map()) in list(using key)) */}
+        {/* {arr.map((element,index)=><b key={index}>{element}</b>)}    */}
+
+        <Maskedtext text={state.wordSelected} guessedLetters={[]}/>
+
         {/* here to work like href,link work like a tag but it doest not refresh the page while switching to another page */}
     <Link to ='/start' className="text-blue-500">Start  Game Link</Link>
         </>
