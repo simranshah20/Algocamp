85f6e73121dcedd0fecb3687bff0ff8950e66824
simran <shahsimran453@gmail.com>
2024-09-25 18:07:02 +0530

    Added functionality to letter buttons

--- Frontend/React_project/Hangman_Game/src/components/LetterButtons/Lettersbuttons.jsx
+++ Frontend/React_project/Hangman_Game/src/components/LetterButtons/Lettersbuttons.jsx
@@ -37,18 +37,24 @@ function LetterButtons({text,guessedLetters,onLetterClick}){
     const guessedLetterSet=new Set(guessedLetters);
 
     const buttonStyle=function(letter){
-        if(guessedLettersSet.has(letter)){
+        if(guessedLetterSet.has(letter)){
             return `${originalLetters.has(letter)?'bg-green-500':'bg-red-500'}`;
         }else{
             return 'bg-blue-500'    
             }
         }
-
+        
+        function handleLetterClick(event){
+         const characterOfTheLetter=event.target.value;
+         onLetterClick?.(characterOfTheLetter)
+        }
+        
     const buttons=ALPHABETS.map(letter=>{
         return(
             <button
             key={`button-${letter}`}
-            onClick={onLetterClick}
+            value={letter}
+            onClick={handleLetterClick}
             disabled={guessedLetterSet.has(letter)}
             className={`h-12 w-12 m-1 text-white rounded-md ${buttonStyle(letter)}`}
             >
--- Frontend/React_project/Hangman_Game/src/pages/playGame.jsx
+++ Frontend/React_project/Hangman_Game/src/pages/playGame.jsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import Maskedtext from "../components/maskedText/MaskedText";
 import LetterButtons from "../components/LetterButtons/Lettersbuttons";
+import { useState } from "react";
 
 function PlayGame(){
 //   const [searchParams]=useSearchParams();
@@ -10,18 +11,22 @@ function PlayGame(){
 
 const  {state}=useLocation();  //destructing state property
 
+const [guessedLetters,setGuessedLetters]=useState([]);
+function handleLetterClick(letter){
+    setGuessedLetters([...guessedLetters,letter]);  //...guessedLetters mean unpacking letter
+}
+
 // const arr=["hello","world"];
     return(
         <>
-        <h1>Play Game {state.wordSelected}</h1>  
-
+        <h1>Play Game</h1>  
         {/* rendering list in reacct (on UI(using map()) in list(using key)) */}
         {/* {arr.map((element,index)=><b key={index}>{element}</b>)}    */}
 
-        <Maskedtext text={state.wordSelected} guessedLetters={[]}/>
+        <Maskedtext text={state.wordSelected} guessedLetters={guessedLetters}/>
 
          <div>
-         <LetterButtons text={state.wordSelected} guessedLetters={[]} onLetterClick={()=>{}}/>
+         <LetterButtons text={state.wordSelected} guessedLetters={guessedLetters} onLetterClick={handleLetterClick}/>
          </div>
         {/* here to work like href,link work like a tag but it doest not refresh the page while switching to another page */}
     <Link to ='/start' className="text-blue-500">Start  Game Link</Link>
