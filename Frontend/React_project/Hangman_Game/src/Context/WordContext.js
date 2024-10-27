823bb36556be0da8bba5bbf83f341642eefe3911
simran <shahsimran453@gmail.com>
2024-10-27 10:25:16 +0530

    Arragement

--- Frontend/React_project/Hangman_Game/src/Context/WordContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const WordContext = createContext(null);       //create a context object and work as common storage
--- Frontend/React_project/PerformanceDemo/src/Modal.jsx
+++
@@ -1,9 +0,0 @@
-export default function modal({ close }){
-    return (
-        <div className="modal">
-          <h2>Modal</h2>
-          <p>Hello everyone</p>
-          <button onClick={ close }>Close Modal</button>
-        </div>
-    )
-}
